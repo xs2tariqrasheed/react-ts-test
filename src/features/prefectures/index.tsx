b164a17440dc181cff7b66ef2b9ecab82f2e4e3e
Tariq Rasheed <xs2tariqrasheed@gmail.com>
2023-06-23 01:11:58 +0500

    prefectures list with checkbox and basic api

--- src/features/prefectures/index.tsx
+++ src/features/prefectures/index.tsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { ALL_PREFECTURES_API, API_KEY, BASE_URL } from "../../constants";
-
 interface Prefecture {
   prefCode: number;
   prefName: string;
 }
 
 const Prefectures = () => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [populationData, setPopulationData] = useState<any[]>([]);
+
+  const [selectedPref, setSelectedPref] = useState<Prefecture[]>([]);
+
+  const BASE_URL = process.env.REACT_APP_BASE_URL || "";
+  const ALL_PREFECTURES_API = process.env.REACT_APP_ALL_PREFECTURES_API;
+  const POPULATION_COMPOSITION_API =
+    process.env.REACT_APP_POPULATION_COMPOSITION_API;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
   useEffect(() => {
+    setIsFetching(true);
     axios
       .get(BASE_URL + ALL_PREFECTURES_API, {
         headers: {
@@ -17,20 +29,76 @@ const Prefectures = () => {
       })
       .then((res) => {
         setData(res?.data?.result);
+        setIsFetching(false);
       })
-      .catch((error) => console.log("error in fetching prefectures", error));
+      .catch((error) => {
+        setIsFetching(false);
+        console.log("error in fetching prefectures", error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const [data, setData] = useState<Prefecture[]>([]);
-  const [selectedPref, setSelectedPref] = useState<Prefecture | null>(null);
+  const fetchPopulationComposition = (prefCode: number) => {
+    setLoading(true);
+    axios
+      .get(
+        BASE_URL +
+          POPULATION_COMPOSITION_API +
+          `?prefCode=${prefCode}&cityCode=-`,
+        {
+          headers: {
+            "X-API-KEY": API_KEY,
+          },
+        }
+      )
+      .then((res) => {
+        setLoading(false);
+        const compositions = res?.data?.result?.data;
+
+        // filtered compositions
+        let filteredCompositions = [] as any;
+        compositions.forEach((item: any) => {
+          const value = {} as any;
+          value[item?.label] = item.data;
+          filteredCompositions?.push(value);
+        });
+
+        // new modified array
+        let newData = {
+          prefCode: prefCode,
+          data: filteredCompositions,
+        };
+        setPopulationData([...populationData, newData]);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log("error in fetching prefectures", error);
+      });
+  };
 
   const handleCheckboxChange = (prefecture: Prefecture) => {
-    setSelectedPref(prefecture);
+    if (
+      populationData.find((item) => item.prefCode === prefecture?.prefCode) !==
+      undefined
+    ) {
+      setPopulationData(
+        populationData.filter((pref) => pref?.prefCode !== prefecture.prefCode)
+      );
+    } else {
+      fetchPopulationComposition(prefecture?.prefCode);
+    }
+    if (selectedPref.includes(prefecture)) {
+      setSelectedPref(selectedPref.filter((pref) => pref !== prefecture));
+    } else {
+      setSelectedPref([...selectedPref, prefecture]);
+    }
   };
 
   return (
     <div style={{ padding: 50 }}>
-      {data?.length > 0 ? (
+      {loading || isFetching ? (
+        <p>Loading...</p>
+      ) : (
         <div>
           <h2>Prefecture List</h2>
           <div
@@ -44,21 +112,18 @@ const Prefectures = () => {
             {data.map((prefecture) => (
               <div key={prefecture.prefCode}>
                 <input
+                  disabled={loading}
                   type="checkbox"
-                  checked={selectedPref?.prefCode === prefecture.prefCode}
+                  checked={selectedPref.includes(prefecture)}
                   onChange={() => handleCheckboxChange(prefecture)}
                 />
                 <span>{prefecture.prefName}</span>
               </div>
             ))}
           </div>
-          {selectedPref && (
-            <p>Selected Prefecture Code: {selectedPref.prefCode}</p>
-          )}
         </div>
-      ) : (
-        <p>no data found</p>
       )}
+      <pre>{JSON.stringify(populationData, null, 2)}</pre>
     </div>
   );
 };
