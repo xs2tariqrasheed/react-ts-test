6a1b83c4786da64c94b71211754ac0e3bc4a8c3c
Tariq Rasheed <xs2tariqrasheed>
2023-06-23 01:08:05 +0500

    prefecture listing has been completed

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = "https://opendata.resas-portal.go.jp/api/v1/";
-export const ALL_PREFECTURES_API = "prefectures";
-export const API_KEY = "9mm2eT30Ehjx2EVY02lCmKr08Nl6Vj415h2wThdS";
